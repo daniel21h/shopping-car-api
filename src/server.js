babc2bcbf335430d18b2053a66109be4a7c5f82e
daniel21h <hesseldaniel21@gmail.com>
2020-08-18 19:35:13 -0300

    feat: finalized code - mvp

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors')
 
 const server = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/shoppingcar', {
   useCreateIndex: true,
 });
 
+server.use(cors())
 server.use(express.json());
 server.use(routes);
 
